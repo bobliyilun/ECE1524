446efe8c8417f5cebaab83a8c3381400b4d825f9
bobliyilun <liyilun@protonmail.com>
2022-12-30 03:26:55 -0500

    adf

--- router.p4app/control_plane/control_plane.py
+++ router.p4app/control_plane/control_plane.py
@@ -83,8 +83,9 @@ class Control_plane(Thread):
     """
     def handle_pkt(self, pkt):
         pkt = Digest_data(bytes(pkt))
-        if not self.pkt_valid(pkt): return
         pkt.show()
+        if not self.pkt_valid(pkt): return
+        
         # pkt is a Scapy packet with the format:
         #   Digest_data() / Ether() / ... payload ...
         # TODO: handle the packet appropriately
