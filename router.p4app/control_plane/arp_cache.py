6571f147b5547eabddc6c47ca75fb43e101bd9bc
bobliyilun <liyilun@protonmail.com>
2022-12-31 21:48:15 -0500

    done commenting

--- router.p4app/control_plane/arp_cache.py
+++ router.p4app/control_plane/arp_cache.py
@@ -37,7 +37,7 @@ class ARP_cache():
         self.sendp = config.sendp
         self.rtable = config.rtable
         self.dma_iface = config.dma_iface
-        self.arp_pending_reply = []
+        self.arp_pending_reply = [] #list of arp request
         
         # TODO: initialize ARP handling thread(s)?
         # TODO: define additional helper methods
@@ -50,8 +50,8 @@ class ARP_cache():
 
     def handle_arp_miss(self, pkt):
         t = Ether()/ARP()
-        if IP in pkt:
-            self.arp_pending_reply.append(pkt) # Counter to determine if it is stale
+        if IP in pkt: # meaning that arp cache did not find the nexthop ip, need to broadcast 
+            self.arp_pending_reply.append(pkt) # add to list of pkts pending reply
             t[Ether].dst = ETH_BROADCAST
             t[Ether].src = pkt[Ether].src
 
@@ -62,16 +62,21 @@ class ARP_cache():
 
         elif ARP in pkt:
             t = pkt
-        
+
+        # add entry to the arp table used in dataplane. (untested)
         self.tables_api.table_cam_add_entry(ARP_CACHE_TABLE_NAME, match_fields={"next_hop_ipv4":  t[ARP].psrc},\
              action_name='MyIngress.arp_respond', action_params={"result": t[ARP].hwsrc})
 
+        # have a serious question about finding which interface the pkt comes from so that we don't send it back to the sender
+        # below is my best attempt at formulating this.
         for i in self.ifaces:
-            if IP in pkt:
+            if IP in pkt: 
+                #send it through every interface of the router other than the one it's arrived from and the source
                 if (i.ip != pkt[IP].dst) and (i.ip != pkt[IP].src):
                     sendp(t, iface=i, verbose=False)
 
             if ARP in pkt:
+                #send it through every interface of the router other than the one it's arrived from and the source
                 if (i.ip != pkt[ARP].psrc) and (i.ip != pkt[ARP].pdst):
                     sendp(t, iface=i, verbose=False)
 
@@ -83,9 +88,14 @@ class ARP_cache():
                 s[Ether].dst = pkt[ARP].hwsrc
                 t = s
                 self.tables_api.table_cam_add_entry(ARP_CACHE_TABLE_NAME, match_fields={"next_hop_ipv4":  s[ARP].psrc},\
-                action_name='MyIngress.arp_respond', action_params={"result": s[ARP].hwsrc})
-                self.arp_pending_reply.remove(s)
-                sendp(t, iface=self.dma_iface, verbose=False)
+                action_name='MyIngress.arp_respond', action_params={"result": s[ARP].hwsrc}) # add to arp table in dataplane
+                self.arp_pending_reply.remove(s) # remove the corresponding pkt pending request
+                sendp(t, iface=self.dma_iface, verbose=False) # send the packt to dataplane
                 return
+
+        # current router did not send a arp request pending reply. Foward the pkt and add entry in arp table regardless.
+        # all of these are untested, just doing my best to guess 
+        self.tables_api.table_cam_add_entry(ARP_CACHE_TABLE_NAME, match_fields={"next_hop_ipv4":  s[ARP].psrc},\
+                action_name='MyIngress.arp_respond', action_params={"result": s[ARP].hwsrc}) # add to arp table in dataplane
         sendp(pkt, iface=self.dma_iface, verbose=False)
 
